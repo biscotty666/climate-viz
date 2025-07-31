9e7de5886b72a1a7489f174e1f266ec78b835659
biscotty666 <biscotty666@gmail.com>
2025-07-31 12:35:13 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Global-mean monthly, seasonal, and annual means, 1880-present, updated through most recent month
+
+Data from https://data.giss.nasa.gov/gistemp/
---
+++ flake.nix
@@ -0,0 +1,72 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-25.05";
+
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+      #packageOverrides = pkgs.callPackage ./python-packages.nix {};
+      #python3 = pkgs.python3.override { inherit packageOverrides; };
+    in {
+      devShell = pkgs.mkShell {
+        name = "python-venv";
+        venvDir = "./.venv";
+        buildInputs = with pkgs; [
+            R
+            chromium
+            pandoc
+            texlive.combined.scheme-full
+            rstudio
+            quarto
+            (with rPackages; [
+              concaveman
+              corrplot
+              gcookbook
+              geomtextpath
+              ggblend
+              ggcorrplot
+              ggdensity
+              ggdist
+              ggforce
+              ggpointdensity
+              ggrepel
+              ggtext
+              ggiraph
+              ggridges
+              gt
+              hexbin
+              igraph
+              mapproj
+              maps
+              MASS
+              pagedown
+              palmerpenguins
+              patchwork
+              quarto
+              remotes
+              rgl
+              sf
+              tidyr
+              tidyverse
+              vcd
+              webshot2
+            ])
+         ];
+
+        shellHook = ''
+            export BROWSER=zen
+            #jupyter lab
+        '';
+
+      };
+    }
+  );
+}
